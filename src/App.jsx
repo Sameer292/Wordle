22c4ccb38d86c7ca2cd0723ef3561b06d338807e
Sameer Paudel <paudelsameer888@gmail.com>
2025-03-16 21:45:05 +0545

    Added basic functionalities like entering and color validating the guesses

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,114 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useEffect, useState } from 'react'
+// import wordJson from './wordle-words.json'
+import wordJson from './word.json'
+// import axios from 'axios';
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const PROXY = "https://cors-anywhere.herokuapp.com/";
+  const API_URL = "https://api.frontendexpert.io/api/fe/wordle-words";
+  const [solution, Setsolution] = useState('');
+  const [guesses, Setguesses] = useState(Array(6).fill(null));
+  const [currentGuess, SetcurrentGuess] = useState('');
+  const [isGameOver, setIsGameOver] = useState(false);
+  useEffect(() => {
+    const getWords = async () => {
+      try {
+        // const response = await axios.get(PROXY + API_URL);
+        // const words = await fetch('/wordle-words.json');
+        // const randomWord = response[Math.floor(Math.random() * response.length)];
+        // Setsolution(randomWord);
+        // console.log(response.data);
+        // console.log(solution);
+
+
+        const response = wordJson;
+        const randomWord = response[Math.floor(Math.random() * response.length)];
+        Setsolution(randomWord);
+        console.log(randomWord);
+      } catch (error) {
+        console.error("Error fetching words:", error);
+      }
+
+    };
+    getWords();
+  }, [])
+  useEffect(() => {
+    function handleKeyDown(e) {
+      if (isGameOver) return;
+
+
+      if (e.key === 'Backspace') {
+        SetcurrentGuess(currentGuess.slice(0, -1))
+        return;
+      } else if (e.key === 'Enter') {
+        if (currentGuess.length !== GUESS_LEGTH || !wordJson.includes(currentGuess)) {
+          return;
+        }
+        // const isCorrect = currentGuess === solution;
+        if (currentGuess === solution) {
+          setIsGameOver(true);
+          // return;
+        }
+        const oldGuesses = [...guesses];
+        oldGuesses[guesses.findIndex(val => val == null)] = currentGuess;
+        Setguesses(oldGuesses);
+        SetcurrentGuess('');
+        // if (isCorrect) {
+        //   setIsGameOver(true);
+        // }
+      } else if (!/^[a-zA-Z]$/.test(e.key)) {
+        return;
+      }
+      if (currentGuess.length >= GUESS_LEGTH) return;
+      SetcurrentGuess(currentGuess + e.key.toLowerCase());
+    }
+    window.addEventListener('keydown', handleKeyDown);
+    return () => {
+      window.removeEventListener('keydown', handleKeyDown);
+    }
+  }, [currentGuess, isGameOver, solution])
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div className='App'>
+      {
+        guesses.map((guess, index) => {
+          const isCurrentGuess = index === guesses.findIndex(val => val == null)
+          return <Line
+            key={index} guess={isCurrentGuess ? currentGuess : guess ?? ''}
+            isFinal={!isCurrentGuess && guess != null}
+            solution={solution}
+          />
+        })
+      }
+    </div>
   )
 }
 
 export default App
+
+
+
+const GUESS_LEGTH = 5;
+function Line({ guess, isFinal, solution }) {
+  const tiles = [];
+  for (let i = 0; i < GUESS_LEGTH; i++) {
+    const char = guess[i]
+    let className = 'tile'
+    if (isFinal) {
+      if (char === solution[i]) {
+        className += ' correct'
+      } else if (solution.includes(char)) {
+        className += ' close'
+      } else {
+        className += ' wrong'
+      }
+    }
+    tiles.push(<div key={i} className={className} >{char}</div>)
+  }
+  return (
+    <div className='line'>
+      {tiles}
+    </div>
+  )
+}
